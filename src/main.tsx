916abbec51e8892f6fd11f9e2de9274eafbd0b72
loading1031 <tjdans1031@gmail.com>
2025-01-25 22:45:36 +0900

    init routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import CheckoutPage from './pages/Checkout' // default로 export한 경우, 중괄호 없음
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <CheckoutPage />,
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
