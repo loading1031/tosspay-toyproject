236bb9a662eabc2290bc083f59732fedf9b9a274
loading1031 <tjdans1031@gmail.com>
2025-01-26 07:24:21 +0900

    bug:  렌더링 에러 및 서버 에러 발생

--- version2/src/pages/Checkout.tsx
+++ version2/src/pages/Checkout.tsx
@@ -1,67 +1,93 @@
 // Checkout.tsx
 
 import { useEffect, useRef, useState } from "react"
-import { loadPaymentWidget, PaymentWidgetInstance } from "@tosspayments/payment-widget-sdk"
-// import { ANONYMOUS } from "@tosspayments/payment-widget-sdk"
 import { nanoid } from "nanoid"
-import "../App.css"
 
-const clientKey = "test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm" // 공개된 테스트 키
+// 모듈 임포트 연동방식
+import { 
+  loadTossPayments, 
+  ANONYMOUS,
+  TossPaymentsSDK,
+  WidgetPaymentMethodWidget,
+  TossPaymentsWidgets,
+} from "@tosspayments/tosspayments-sdk"
+
+const clientKey = "test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm";
 const customerKey = "YbX2HuSlsC9uVJW6NMRMj"
 
 export default function App() {
-  const paymentWidgetRef = useRef<PaymentWidgetInstance | null>(null)
-  const paymentMethodsWidgetRef = useRef<ReturnType<
-    PaymentWidgetInstance["renderPaymentMethods"]
-  > | null>(null)
+  const paymentWidgetRef = useRef<TossPaymentsSDK | null>(null) // TossPaymentsWidgets로 변경
+  const paymentMethodWidgetRef = useRef<WidgetPaymentMethodWidget | null>(null); // Promise 해제 후 저장
+  const widgetRef = useRef<TossPaymentsWidgets | null>(null);
   const [price, setPrice] = useState(45_000) // 큰 숫자에는 '_'로 가독성을 높임
 
+  const updateWidgets = async (widgets: TossPaymentsWidgets, price: number) => {
+    await widgets.setAmount({
+      currency: "KRW",
+      value: price,
+    });
+  
+    const newPaymentMethodWidget = await widgets.renderPaymentMethods({
+      selector: "#payment-method",
+      variantKey: "DEFAULT",
+    });
+  
+    return newPaymentMethodWidget;
+  };
+  
   useEffect(() => {
     (async () => {
-      const paymentWidget = await loadPaymentWidget(clientKey, customerKey)
+      const tossPayments = await loadTossPayments(clientKey);
+      const widgets = tossPayments.widgets({ customerKey }); // 회원 결제
+      // const widgets = await tossPayments.widgets({ customerKey: ANONYMOUS});
 
-      const paymentMethodsWidget = paymentWidget.renderPaymentMethods(
-        "#payment-widget",
-        price
-      )
-
-      paymentWidgetRef.current = paymentWidget
-      paymentMethodsWidgetRef.current = paymentMethodsWidget
+      paymentMethodWidgetRef.current = await updateWidgets(widgets, price);
+      paymentWidgetRef.current = tossPayments;
+      widgetRef.current = widgets;
     })()
   }, [])
 
   useEffect(() => {
-    const paymentMethodsWidget = paymentMethodsWidgetRef.current
+    (async () => {
+      const widgets = widgetRef.current;
+      if ( widgets == null) {
+        return
+      } 
 
-    if (paymentMethodsWidget == null) {
-      return
-    }
+      const paymentMethodWidget = paymentMethodWidgetRef.current;
+      if ( paymentMethodWidget != null) {
+        try {
+          await paymentMethodWidget.destroy();
+          paymentMethodWidgetRef.current = null;
+        } catch (error) {
+          console.error("Failed to destroy the payment widget:", error);
+        }
+      }
 
-    paymentMethodsWidget.updateAmount(
-      price,
-      paymentMethodsWidget.UPDATE_REASON.COUPON
-    )
+      // 새 위젯 참조 저장
+      paymentMethodWidgetRef.current = await updateWidgets(widgets, price);
+    })()
   }, [price])
 
   return (
     <div className="App">
       <h1>주문서</h1>
-      <div id="payment-widget" />
+      <div id="payment-method" />
       <div>
         <input
           type="checkbox"
           onChange={(event) => {
-            setPrice(event.target.checked ? price - 5_000 : price + 5_000)
+            setPrice(event.target.checked ? price - 5_000 : price + 5_000)            
           }}
         />
         <label>5,000원 할인 쿠폰 적용</label>
       </div>
       <button
           onClick={async () => {
-            const paymentWidget = paymentWidgetRef.current
+            const widgets = widgetRef.current;
 
             try {
-              await paymentWidget?.requestPayment({
+              await widgets?.requestPayment({
                 orderId: nanoid(),
                 orderName: "토스 티셔츠 외 2건",
                 customerName: "김토스",
