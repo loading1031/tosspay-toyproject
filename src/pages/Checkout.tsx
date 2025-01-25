56b7b0efdde1a5b1fd11f83ca7aaeba2432b678a
loading1031 <tjdans1031@gmail.com>
2025-01-26 00:53:15 +0900

    feat: 할인 쿠폰 적용

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,6 +1,6 @@
 // Checkout.tsx
 
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import { loadPaymentWidget, PaymentWidgetInstance } from "@tosspayments/payment-widget-sdk"
 // import { ANONYMOUS } from "@tosspayments/payment-widget-sdk"
 import { nanoid } from "nanoid"
@@ -11,22 +11,51 @@ const customerKey = "YbX2HuSlsC9uVJW6NMRMj"
 
 export default function App() {
   const paymentWidgetRef = useRef<PaymentWidgetInstance | null>(null)
-  const price = 50_000
+  const paymentMethodsWidgetRef = useRef<ReturnType<
+    PaymentWidgetInstance["renderPaymentMethods"]
+  > | null>(null)
+  const [price, setPrice] = useState(45_000) // 큰 숫자에는 '_'로 가독성을 높임
 
   useEffect(() => {
     (async () => {
       const paymentWidget = await loadPaymentWidget(clientKey, customerKey)
 
-      paymentWidget.renderPaymentMethods("#payment-widget", price)
+      const paymentMethodsWidget = paymentWidget.renderPaymentMethods(
+        "#payment-widget",
+        price
+      )
 
       paymentWidgetRef.current = paymentWidget
+      paymentMethodsWidgetRef.current = paymentMethodsWidget
     })()
   }, [])
+
+  useEffect(() => {
+    const paymentMethodsWidget = paymentMethodsWidgetRef.current
+
+    if (paymentMethodsWidget == null) {
+      return
+    }
+
+    paymentMethodsWidget.updateAmount(
+      price,
+      paymentMethodsWidget.UPDATE_REASON.COUPON
+    )
+  }, [price])
 
   return (
     <div className="App">
       <h1>주문서</h1>
       <div id="payment-widget" />
+      <div>
+        <input
+          type="checkbox"
+          onChange={(event) => {
+            setPrice(event.target.checked ? price - 5_000 : price + 5_000)
+          }}
+        />
+        <label>5,000원 할인 쿠폰 적용</label>
+      </div>
       <button
           onClick={async () => {
             const paymentWidget = paymentWidgetRef.current
